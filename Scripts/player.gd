b8ca6c142d1cb9a45e3033ea46d730d5772f5577
Matheus de Araujo Farina <farinamatheus07@gmail.com>
2024-08-16 17:52:46 -0300

    adjustments being done to player scene - might branch later

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,45 +1,17 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -900.0
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+const SPEED = 300.00
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-		
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-	# Flip the Sprite
-	if direction > 0:
-		animated_sprite_2d.flip_h = false
-	elif direction < 0:
-		animated_sprite_2d.flip_h = true
-		
-	# Play Animations
-	if is_on_floor():
-		if direction == 0:
-			animated_sprite_2d.play("IDLE")
-		else:
-			animated_sprite_2d.play("WALK")
-	else:
-		animated_sprite_2d.play("JUMP")
-
-# 16/08/24 - Trying to figure this animation... and the falling one...
-func attack():
-	$AnimatedSprite2D.animation = "ATTACK"
-		
-
+func _physics_process(delta):
+	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
+	velocity = direction * SPEED
 	move_and_slide()
+	
+	if direction.x < 0 and direction.y == 0:
+		$AnimatedSprite2D.play("walk_left")
+	elif direction.x > 0 and direction.y == 0:
+		$AnimatedSprite2D.play("walk_right")
+	elif direction.y < 0:
+		$AnimatedSprite2D.play("walk_up")
+	elif direction.y > 0:
+		$AnimatedSprite2D.play("walk_down")
